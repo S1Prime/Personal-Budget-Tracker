fca33af2449d8ddfd45e232f802e8e662214be11
S1Prime <vaishnavvenu2007@gmail.com>
2026-06-11 19:00:25 +0530

    feat: implement budget tracking logic including viewing, totaling, and categorized expense storage

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 #Personal Budget Tracker
 
-expenses=[] #list of expenses in form of dictionaries
+expenseslst=[] #list of expenses in form of dictionaries
 print("Welcome to Personal Budget Tracker")
 
 while True:
     print("===Menu===")
     print("1.Add Expenses")
-    print("2.Exit Menu")
-    print("3.View Expenses")
-    print("4.View Total Expenses")
-
+    print("2.View Expenses")
+    print("3.View Total Expenses")
+    print("4.Exit Menu")
+#1.Adding Expenses
     choice = input("Enter Your Choice:")
     if choice == "1":
         date = int(input("Enter the date:"))
@@ -20,10 +20,38 @@ while True:
             "Date": date,
             "category": category,
             "Description": desc,
-            "Amount Spend": amt
+            "Amount": amt
         }
-        expenses.append(expense)
-    if (choice==2):
-    if (choice==3):
-    if (choice==4):
+        expenseslst.append(expense)#for storing the data in expenses 
+        print("\n Expenses Added Succesfully")
+
+#2.Viewing Expenses
+    elif (choice==2):
+        if (len(expenseslst)==0):
+            print("No expenses found")
+        else:
+            print("===This is the expenses===")
+            count=1
+            for exp in expenses:
+                print(f"Expense No {count}=> {exp["Date"]},{exp["category"]},{exp["Description"]},{exp["Amount"]} ")
+                count+=1
+
+#3.Viewing Total Expenses
+    elif (choice==3):
+        total=0
+        for krcha in expenseslst:
+            total=total + krcha["Amount"]
+        print("\n Total Expense is :",total)
+
+#4.Exiting the Menu
+    elif (choice==4):
+        print("Thank You for using Personal Budget Tracker")
+        break
+
+    else:
+        print("Invalid Choice.TRY AGAIN")
+
+
+
+        
 
