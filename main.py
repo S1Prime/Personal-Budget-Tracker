6d6e0853226f74fd9579d6c6957c1e810b312c41
S1Prime <vaishnavvenu2007@gmail.com>
2026-06-11 18:28:25 +0530

    feat: initialize core menu structure and expense input logic for budget tracker

---
+++ main.py
@@ -0,0 +1,22 @@
+#Personal Budget Tracker
+
+expenses=[] #list of expenses in form of dictionaries
+print("Welcome to Personal Budget Tracker")
+
+while True:
+    print("===Menu===")
+    print("1.Add Expenses")
+    print("2.Exit Menu")
+    print("3.View Expenses")
+    print("4.View Total Expenses")
+
+    choice=input("Enter Your Choice:")
+    if (choice==1):
+        date=int(input("Enter the date:"))
+        category=input("Enter the type of Expense(Food,Travel,Stationery)")
+        desc=input("Enter your description:")
+        amt=int(input("Enter the amount:"))
+    if (choice==2):
+    if (choice==3):
+    if (choice==4):
+
